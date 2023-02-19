11c79dfa6f3b27ab4c6b5a313cdf095d90374e88
gustavostr <gao.gustavo@gmail.com>
2023-02-19 18:18:50 -0300

    comentário adicional

--- ex005.py
+++ ex005.py
@@ -1,4 +1,6 @@
-n = float (input("Digite um número: "))
+n = float(input("Digite um número: "))
 s = n+1
 a = n-1
 print("O sucessor é {} e o antecessor é {} ".format(s,a))
+#Pode-se utilizar o seguinte código  print("O sucessor é {} e o antecessor é {} ".format(s,(n-1),(n+2))).
+#apenas se não for preciso utilizar as variáveis "s" e "a" em partes posteriores do código#
