6ffd51e493d58a267d320e7f631c96dca9ee6253
gustavostr <gao.gustavo@gmail.com>
2023-03-10 23:11:41 -0300

    ortografia

--- ex004.py
+++ ex004.py
@@ -1,7 +1,7 @@
 d = input('Digite algo: ')
 print("Tem apenas números ?",d.isnumeric())
 print(d.isalnum())
-print(d.isalpha())
+print("É letras ? ",d.isalpha())
 print(d.islower())
 print(d.isdecimal())
 
