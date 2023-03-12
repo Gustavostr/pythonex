c57e180662edb11fb1c6119d1bbda0a274104c52
gustavostr <gao.gustavo@gmail.com>
2023-03-11 22:55:41 -0300

    Adicionando mais um exercício

---
+++ ex021.py
@@ -0,0 +1,13 @@
+#import pygame
+#pygame.mixer.init()
+#pygame.init()
+#pygame.mixer.music.load('JJD - Adventure [NCS Release].mp3')
+#pygame.mixer.music.play()   não funcionou e não entedi o motivo, não apresenta erro no console
+import playsound
+playsound.playsound('JJD - Adventure [NCS Release].mp3')
+#instalei o módulo playsound e funcionou.
+
+
+
+
+
