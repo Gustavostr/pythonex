3824136d892970c6d170c7a035b3cada7fb6a395
gustavostr <gao.gustavo@gmail.com>
2023-03-22 21:33:19 -0300

    Melhoria do código

--- ex016.py
+++ ex016.py
@@ -3,5 +3,8 @@ num = float(input('Digite um numero qualquer para obter sua parte inteira: '))
 par = math.trunc(num)
 print('A parte inteira de {} é {}'.format(num, par))
 
+# pode-se simplificar ainda mais colocando a função math.trunc no format do print assim:
+# print('A parte inteira de {} é {}'.format(num, math.trunc(num))
+# sendo assim não precisa-se alocar espaço na memória criando uma nova variável 
 
 
