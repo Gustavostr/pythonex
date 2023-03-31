599b06735c41521681eb1766f99e593ae8e97dd2
gustavostr <gao.gustavo@gmail.com>
2023-03-31 16:30:11 -0300

    Otimização do código

--- ex016.py
+++ ex016.py
@@ -1,10 +1,11 @@
 import math
 num = float(input('Digite um numero qualquer para obter sua parte inteira: '))
-par = math.trunc(num)
-print('A parte inteira de {} é {}'.format(num, par))
+print('A parte inteira de {} é {}'.format(num, math.trunc(num)))
+#par = math.trunc(num)
+#print('A parte inteira de {} é {}'.format(num, par))
 
 # pode-se simplificar ainda mais colocando a função math.trunc no format do print assim:
-# print('A parte inteira de {} é {}'.format(num, math.trunc(num))
+
 # sendo assim não precisa-se alocar espaço na memória criando uma nova variável 
 
 
