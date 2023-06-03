46c64e6c346b5272dbadbd2bf7d356572f12e914
gustavostr <gao.gustavo@gmail.com>
2023-06-02 22:13:25 -0300

    ajuste do código

--- ex024.py
+++ ex024.py
@@ -1,1 +1,7 @@
-gfg = "d"
+cidade = str(input("Digite o nome de uma cidade: "))
+cidade = cidade.title()
+cidade = cidade.split()
+if cidade[0] == str("Santo"):
+	print("\n Essa cidade é santa")
+else:  
+	print("\n essa cidade, infelizmente, não é santa")
