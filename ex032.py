e2cb13cf3f4632046a4d42012d1ecdb8bdc6f9ab
gustavostr <gao.gustavo@gmail.com>
2023-08-23 23:26:16 -0300

    teste

---
+++ ex032.py
@@ -0,0 +1,8 @@
+
+ano_usuario = int(input("Digite o ano que você quer conferir se é bissexto: "))
+if ano_usuario%4 == 0:
+    print("o ano {} é bissexto!")
+else:
+        if ano_usuario%100 == 0:
+            print ("algo")
+
