793ac4904c976c81d8a2feb3aabbd89affd759c7
gustavostr <gao.gustavo@gmail.com>
2023-09-18 21:36:59 -0300

    Adicionando mais exercícios

---
+++ ex033.py
@@ -0,0 +1,6 @@
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite um número: "))
+num3 = float(input("Digite um número: "))
+vet = [num1, num2, num3]
+vet.sort ()
+print ("O maior número é {} e o menor é {}".format(vet[2],vet[0]))
