646222b758a16e71b050b5d922a018d2f8672e59
Gustavostr <gao.gustavo@gmail.com>
2024-12-28 16:45:10 -0300

    Update ex072.py

--- ex072.py
+++ ex072.py
@@ -1,11 +1,13 @@
-extenso = ('um', 'dois', 'três', 'quatro', 'cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
-numero = 0
+extenso = ('zero','um', 'dois', 'três', 'quatro', 'cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+numero = 80
 
-while numero < 1 or numero > 20:
+while numero < 0 or numero > 20:
     try:
         numero = int(input("Digite um número inteiro de 0 a 20! "))
-        print(f"você digitou o número {extenso[numero-1]}!")
+        print(f"você digitou o número {extenso[numero]}!")
     except ValueError:
         print(" Erro! Digite um numero inteiro ")
+    except IndexError:
+        print("Digite um numero entre 0 e 20! ")
 
 
